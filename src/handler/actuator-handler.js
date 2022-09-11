848ab5cee28632262ed80d3656111dcf13e587cb
dhifafaz <dhifaf.az@gmail.com>
2022-09-11 07:25:24 +0700

    fixing main to origin

--- src/handler/actuator-handler.js
+++
@@ -1,7 +0,0 @@
-const { image } = require("../config/cloudinary");
-const pool = require("../config/db");
-const { uploadImage, deleteImage } = require("../utils/cloudinary");
-
-const uploadActuator = async (request, h) => {
-    
-};
