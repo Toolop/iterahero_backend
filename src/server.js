974e211bdcda39cc538729d8cefed74b73f8e558
Toolop <toolop2312@gmail.com>
2022-09-19 11:58:12 +0700

    fix conflict

--- src/server.js
+++ src/server.js
@@ -1,21 +1,14 @@
 const Hapi = require('@hapi/hapi');
 const dotenv = require('dotenv');
 const routes = require('./route');
-<<<<<<< HEAD
-=======
 const jwt = require('hapi-auth-jwt2');
 const {validate} = require('./utils/jwt-utils');
->>>>>>> 23486b345a014c14802c09f4c4da3ce7af1ba84b
 
 const init = async () =>{
     dotenv.config();
 
     const server = Hapi.server({
-<<<<<<< HEAD
         port: process.env.PORT || 80,
-=======
-        port: process.env.PORT || 3000,
->>>>>>> 23486b345a014c14802c09f4c4da3ce7af1ba84b
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     });
 
