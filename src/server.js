a99bc4689737954fe1fc26140be574b4de2e6a61
Toolop <toolop2312@gmail.com>
2022-09-27 11:10:02 +0700

    fixing

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const init = async () =>{
 
     server.auth.strategy('jwt', 'jwt',
     { key: process.env.JWT_SECRET, // Never Share your secret key
-      expiresIn: '365d',
+      expiresIn: '1h',
       validate:validate
     });
 
