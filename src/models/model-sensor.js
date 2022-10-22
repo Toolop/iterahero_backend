3af50ca00d1f75f88379ea58a82e765d94a20c25
Toolop <toolop2312@gmail.com>
2022-10-22 21:28:43 +0700

    adding

--- src/models/model-sensor.js
+++ src/models/model-sensor.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-var current = new Date();
-const timeStamp = new Date(Date.UTC(current.getFullYear(), 
-current.getMonth(),current.getDate(),current.getHours(), 
-current.getMinutes(),current.getSeconds(), current.getMilliseconds()));
-
 
 const sensorSchema = new Schema({
     value: Number,
     id_sensor: Number,
-    createdAt:{ type: Date, default : timeStamp }
-});
+},{ timestamps: true });
 
 module.exports = mongoose.model('sensor', sensorSchema);
