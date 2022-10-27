b0c0d4a124f1dce7624a2c5167e1b3d51b2d0989
Toolop <toolop2312@gmail.com>
2022-10-27 23:15:48 +0700

    adding status

--- src/models/model-sensor.js
+++ src/models/model-sensor.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const sensorSchema = new Schema({
     value: Number,
     id_sensor: Number,
+    status: String,
 },{ timestamps: true });
 
 module.exports = mongoose.model('sensor', sensorSchema);
