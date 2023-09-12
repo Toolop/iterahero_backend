dd337ab8f67ddea5ef7a7b265600db4cdd792a71
Revanfz <revanfauzi81@gmail.com>
2023-09-12 12:17:51 +0700

    Fix prisma untuk mengambil data log kamera berdasarkan id kamera

--- src/controller/camera-log/handler/getHandler.js
+++ src/controller/camera-log/handler/getHandler.js
@@ -3,18 +3,44 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getCameraLog = async (request, h) => {
-    const { id_camera } = request.params;
+    const { id_camera } = request.query;
+    let { page, size } = request.query;
     let response;
 
     try {
-        data = await prisma.camera_log.findMany({
-            where: { id_camera: id_camera }
+        page = parseInt(page) || 1;
+        size = parseInt(size) || 10;
+        let  offset = (page - 1) * size;
+        
+        const totalData = await prisma.camera_log.count({
+            where: { id_camera: parseInt(id_camera) },
+        });
+        
+        const pagination = {
+            totalPage: Math.floor(totalData / size) + 1,
+            totalData: parseInt(totalData),
+            currentPage: page,
+            size
+        }
+
+        if (offset > pagination.totalData) {
+            pagination.currentPage = pagination.totalPage;
+        }
+
+        let data = await prisma.camera.findUnique({
+            where: { id_camera: parseInt(id_camera) },
+            include: {
+                Camera_log: true
+            }
         })
+
         response = h.response({
             status: 'success',
             data,
+            pagination
         }).code(200)
     } catch (e) {
+        console.log(e);
         response = h.response({
             status: 'failed',
             message: e
